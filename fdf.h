59be711e1f61b26012a5a2f3c88a231eca5cab1f
ZomDev3343 <contact.zomzd@gmail.com>
2024-02-08 12:35:01 +0100

    norm

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: tohma <tohma@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 15:32:18 by truello           #+#    #+#             */
-/*   Updated: 2024/02/02 17:01:10 by tohma            ###   ########.fr       */
+/*   Updated: 2024/02/08 12:34:48 by tohma            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,12 @@ typedef struct s_map
 	int	**grid;
 }	t_map;
 
-void	free_map(t_map *map);
-t_map	*create_map(int width, int height);
-int		parse_map_dimension(char *map_file, int *width, int *height);
-int		fill_grid_values(t_map *map, char *map_file);
-t_map	*parse_map(char *map_file);
-void	print_map_infos(t_map *map);
-
 typedef struct s_vector
 {
-    int x;
-    int y;
-    int z;
-}   t_vector;
+	int	x;
+	int	y;
+	int	z;
+}	t_vector;
 
 typedef struct s_mat
 {
@@ -67,23 +60,30 @@ typedef struct s_vars
 
 typedef struct s_img
 {
-    void	*img;
+	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
 	t_vars	*vars;
-}   t_img;
+}	t_img;
 
 typedef struct s_camera
 {
-    int fov;
-    int far;
-    int aspect;
+	int	fov;
+	int	far;
+	int	aspect;
 	int	angle;
-    int x;
-    int y;
-}   t_camera;
+	int	x;
+	int	y;
+}	t_camera;
+
+void		free_map(t_map *map);
+t_map		*create_map(int width, int height);
+int			parse_map_dimension(char *map_file, int *width, int *height);
+int			fill_grid_values(t_map *map, char *map_file);
+t_map		*parse_map(char *map_file);
+void		print_map_infos(t_map *map);
 
 int			manage_input(int keycode, t_vars *vars);
 
@@ -97,8 +97,8 @@ t_camera	make_camera(void);
 int			render_frame(t_vars *vars);
 
 /* Draw */
-void    	draw_pixel(t_img *img, int x, int y, int color);
-void    	draw_square(t_img *img, int x, int y, int size);
+void		draw_pixel(t_img *img, int x, int y, int color);
+void		draw_square(t_img *img, int x, int y, int size);
 void		draw_line(t_img *img, t_vector from, t_vector to);
 
 /* Interact */
--- interact.c
+++ interact.c
@@ -1,30 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   interact.c                                              :+:      :+:    :+:   */
+/*   interact.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: truello <truello@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tohma <tohma@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/11 15:32:18 by truello           #+#    #+#             */
-/*   Updated: 2024/01/18 16:34:04 by truello          ###   ########.fr       */
+/*   Created: 2024/02/08 12:31:10 by tohma             #+#    #+#             */
+/*   Updated: 2024/02/08 12:32:35 by tohma            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-t_interact  create_interact(int x, int y, int size_x, int size_y)
+t_interact	create_interact(int x, int y, int size_x, int size_y)
 {
-    t_interact  res;
+	t_interact	res;
 
-    res.x = x;
-    res.y = y;
-    res.size_x = size_x;
-    res.size_y = size_y;
-    return (res);
+	res.x = x;
+	res.y = y;
+	res.size_x = size_x;
+	res.size_y = size_y;
+	return (res);
 }
 
-void    center_pos(t_vars *vars, t_interact *obj)
+void	center_pos(t_vars *vars, t_interact *obj)
 {
-    obj->x = vars->win_width / 2 - obj->size_x / 2;
-    obj->y = vars->win_height / 2 - obj->size_y / 2;
+	obj->x = vars->win_width / 2 - obj->size_x / 2;
+	obj->y = vars->win_height / 2 - obj->size_y / 2;
 }
